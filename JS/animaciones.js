dde436a536504fedfb3abecc4a2673596490a423
ninOS2341 <alanaaronlpezsamperio@gmail.com>
2024-11-29 09:10:31 -0600

    V1.02
    Co-authored-by: hiram13541@gmail.com <hiram13541@gmail.com>

--- JS/animaciones.js
+++ JS/animaciones.js
@@ -1,45 +1,60 @@
 MenuDentro = () => {
     var menu = document.getElementById('Menu');
+    const divMenu = document.createElement('div');
+     
 
-    menu.style.width = '20rem'
+    divMenu.id = 'divMenu';
+
+    menu.className = 'menuExt';
 
     document.getElementById('Menu').replaceWith(menu);
+    document.getElementById('TituloMenu').remove();
+    
 }
 
 MenuFuera = () => {
     var menu = document.getElementById('Menu');
-    menu.style.width = '100%'
+    const TituloMenu = document.createElement('p');
+    
+    TituloMenu.id = 'TituloMenu';
+    TituloMenu.innerText = 'Menú';  
+    menu.className = 'menuCon';
 
     document.getElementById('Menu').replaceWith(menu);
+    document.getElementById('Menu').appendChild(TituloMenu);
+    document.getElementById('divMenu').remove();
 }
 MenuinfoDentro = () => {
-    
+
 
     if (document.getElementById('divInfo')) {
         console.log("Ya existe el div");
     } else {
         var pag = document.getElementById("Pagp");
-        const divInfo=document.createElement('div')
-        const titulo = document.createElement('div');
+
         const cambio = document.createElement('a');
         const btnSalir = document.createElement('button');
         const icono = document.createElement('i');
-        
+        const divInfo = document.createElement('div');
+        const titulo = document.createElement('div');
         const divTitulo = document.createElement('div');
         const divCuerpo = document.createElement('div');
         const divPie = document.createElement('div');
+        const divOpciones = document.createElement('div');
 
         divInfo.id = 'divInfo';
         divTitulo.id = 'infoT';
         divCuerpo.id = 'infoC';
         divPie.id = 'infoP';
+        btnSalir.id = 'infoBtn';
+        divOpciones.id = 'divOpciones';
 
 
         titulo.innerText = 'Opciones';
         cambio.innerText = 'Cambiar de cuenta';
+        btnSalir.innerText = 'Salir';
 
         icono.className = 'fa-solid fa-user';
-
         cambio.className = 'infoOp';
         cambio.href = '../index.html'
 
@@ -50,8 +65,9 @@ MenuinfoDentro = () => {
 
         divTitulo.appendChild(titulo);
 
-        divCuerpo.appendChild(icono);
-        divCuerpo.appendChild(cambio);
+        divCuerpo.appendChild(divOpciones);
+        divOpciones.appendChild(icono);
+        divOpciones.appendChild(cambio);
 
         divPie.appendChild(btnSalir);
 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PitProyect
 Proyecto de 5to semestre
+Integrantes:
+-Duran Hernandez Diego Andres
+-Hernandez Mendoza Jesus Hiram
+-Islas Rosales Josue
+-Lopez Samperio Alan Aaron
+-Sosa Sevilla Ethan Emmanuel
+
+Proyecto:Gestor para la ayuda de administracion de una empresa
+
+Proposito:El propósito principal del proyecto es desarrollar una herramienta integral que facilite la administración de tareas y procesos en una empresa a través de un sistema de gestión con dos vistas diferenciadas: una para trabajadores y otra para administradores. Este sistema tiene como objetivo mejorar la eficiencia operativa, optimizar la comunicación entre los diferentes niveles de la organización y proporcionar un control más eficiente de los recursos y actividades.
